06cdd3779fe7cfec80f5b21d5b43fb8db12ccd05
Nate Irvin <irvinn@byu.edu>
2025-11-12 22:07:58 -0700

    refactoring

--- client/src/main/java/Main.java
+++ client/src/main/java/Main.java
@@ -4,9 +4,14 @@ import java.util.logging.*;
 
 public class Main
 {
+    private static AppState appState;
+    private static MenuCommandHandlerFactory menuCommandFactory;
+
     public static void main(String[] args)
     {
         System.out.println("Welcome to the Chess app!");
+
+        appState = new AppState();
 
         LogManager.getLogManager().reset();
         Logger logger = Logger.getLogger("default");
@@ -14,42 +19,45 @@ public class Main
         try (ServerFacade serverFacade = new ServerFacade(new SerializerFactory().getGson());
              ConsoleReader consoleReader = new ConsoleReader())
         {
-            AppState appState = new AppState();
             GameListDisplay gameListDisplay = new GameListDisplay(appState, serverFacade);
-            var menuCommandFactory = new MenuCommandHandlerFactory(appState, logger, serverFacade, gameListDisplay);
+            menuCommandFactory = new MenuCommandHandlerFactory(appState, logger, serverFacade, gameListDisplay);
 
             logger.addHandler(new FileHandler("chess-app.log", true));
             serverFacade.bindTo("localhost", 8080);
 
-            while (true) {
-                System.out.print("CHESS [" + appState.currentUsername() + "] $ ");
-                consoleReader.read();
-
-                MenuCommandHandler command;
-                if (!appState.userIsLoggedIn()) {
-                    command = menuCommandFactory.getPreLoginCommand(consoleReader.firstToken());
-                } else {
-                    command = menuCommandFactory.getPostLoginCommand(consoleReader.firstToken());
-                }
-
-                if (command == null)   //user selected to quit
-                {
-                    System.out.println("Have a great day!");
-                    System.out.println();
-                    return;
-                }
-
-                String errorMessage = command.execute(consoleReader.allButFirstToken());
-
-                if (errorMessage != null) {
-                    InvalidMenuCommandHandler.print(errorMessage);
-                }
-            }
+            runUsing(consoleReader);
         }
         catch (Exception e)
         {
             logger.log(Level.SEVERE, "Critical failure", e);
             System.out.println("A critical error has occurred.");
+        }
+    }
+
+    private static void runUsing(ConsoleReader consoleReader) {
+        while (true) {
+            System.out.print("CHESS [" + appState.currentUsername() + "] $ ");
+            consoleReader.read();
+
+            MenuCommandHandler command;
+            if (!appState.userIsLoggedIn()) {
+                command = menuCommandFactory.getPreLoginCommand(consoleReader.firstToken());
+            } else {
+                command = menuCommandFactory.getPostLoginCommand(consoleReader.firstToken());
+            }
+
+            if (command == null)   //user selected to quit
+            {
+                System.out.println("Have a great day!");
+                System.out.println();
+                return;
+            }
+
+            String errorMessage = command.execute(consoleReader.allButFirstToken());
+
+            if (errorMessage != null) {
+                InvalidMenuCommandHandler.print(errorMessage);
+            }
         }
     }
 }
