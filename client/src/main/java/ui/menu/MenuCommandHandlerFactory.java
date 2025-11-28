91aaf798a748b41d9c0bdceb7d8948fa5c2ec622
Nate Irvin <irvinn@byu.edu>
2025-11-28 15:26:27 -0700

    refactoring

--- client/src/main/java/ui/menu/MenuCommandHandlerFactory.java
+++ client/src/main/java/ui/menu/MenuCommandHandlerFactory.java
@@ -26,16 +26,6 @@ public class MenuCommandHandlerFactory
 
     public MenuCommandHandler getPreLoginCommand(String commandName)
     {
-        return getMenuCommand(commandName, false);
-    }
-
-    public MenuCommandHandler getPostLoginCommand(String commandName)
-    {
-        return getMenuCommand(commandName, true);
-    }
-
-    private MenuCommandHandler getMenuCommand(String commandName, boolean isSecured)
-    {
         if(commandName == null)
         {
             commandName = "";
@@ -43,45 +33,47 @@ public class MenuCommandHandlerFactory
 
         switch (commandName.toLowerCase()) {
             case "register" -> {
-                if (!isSecured) {
-                    return new RegisterUserCommandHandler(appState, logger, serverFacade, mainRenderer);
-                }
+                return new RegisterUserCommandHandler(appState, logger, serverFacade, mainRenderer);
             }
             case "login" -> {
-                if (!isSecured) {
-                    return new LoginUserCommandHandler(appState, logger, serverFacade, mainRenderer);
-                }
-            }
-            case "logout" -> {
-                if(isSecured) {
-                    return new LogoutUserCommandHandler(appState, logger, serverFacade, mainRenderer);
-                }
-            }
-            case "create" -> {
-                if(isSecured) {
-                    return new CreateGameCommandHandler(appState, logger, serverFacade, mainRenderer);
-                }
-            }
-            case "list" -> {
-                if(isSecured) {
-                    return new ListGameCommandHandler(logger, mainRenderer);
-                }
-            }
-            case "observe" -> {
-                if(isSecured) {
-                    return new ObserveGameCommandHandler(gameListAccessor, mainRenderer);
-                }
-            }
-            case "join" -> {
-                if(isSecured) {
-                    return new JoinGameCommandHandler(appState, logger, serverFacade, gameListAccessor, mainRenderer);
-                }
+                return new LoginUserCommandHandler(appState, logger, serverFacade, mainRenderer);
             }
             case "help" -> {
-                if(isSecured) {
-                    return new PostloginHelpCommandHandler(mainRenderer);
-                }
                 return new PreloginHelpCommandHandler(mainRenderer);
+            }
+            case "quit", "exit" -> {
+                return null;
+            }
+        }
+
+        return new InvalidMenuCommandHandler(mainRenderer);
+    }
+
+    public MenuCommandHandler getPostLoginCommand(String commandName)
+    {
+        if(commandName == null)
+        {
+            commandName = "";
+        }
+
+        switch (commandName.toLowerCase()) {
+            case "logout" -> {
+                return new LogoutUserCommandHandler(appState, logger, serverFacade, mainRenderer);
+            }
+            case "create" -> {
+                return new CreateGameCommandHandler(appState, logger, serverFacade, mainRenderer);
+            }
+            case "list" -> {
+                return new ListGameCommandHandler(logger, mainRenderer);
+            }
+            case "observe" -> {
+                return new ObserveGameCommandHandler(gameListAccessor, mainRenderer);
+            }
+            case "join" -> {
+                return new JoinGameCommandHandler(appState, logger, serverFacade, gameListAccessor, mainRenderer);
+            }
+            case "help" -> {
+                return new PostloginHelpCommandHandler(mainRenderer);
             }
             case "quit", "exit" -> {
                 return null;
