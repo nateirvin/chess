61d19ccb901ae4e8f05cbc434cd9898ac398a3e4
Nate Irvin <irvinn@byu.edu>
2025-12-02 21:23:55 -0700

    Revert "making screen output thread-safe"
    This reverts commit cf6adf74344c7f6caf1182d12456455ebd2c4934.

--- client/src/main/java/ui/BufferedRenderer.java
+++ client/src/main/java/ui/BufferedRenderer.java
@@ -5,21 +5,14 @@ import chess.ChessGame;
 import ui.data.GameListAccessor;
 import java.io.Closeable;
 import java.io.IOException;
-import java.util.Queue;
-import java.util.concurrent.SynchronousQueue;
 
 public class BufferedRenderer implements Closeable {
     private final ConsoleReader reader;
     private ChessBoardRenderer boardRenderer;
     private GameListRenderer gameListRenderer;
 
-    private boolean inPrompt;
-    private GameInfo pendingBoard;
-    private final Queue<ScreenMessage> pendingMessages;
-
     public BufferedRenderer() {
         reader = new ConsoleReader();
-        pendingMessages = new SynchronousQueue<>();
     }
 
     public void using(GameListAccessor gameListAccessor) {
@@ -27,28 +20,9 @@ public class BufferedRenderer implements Closeable {
         this.gameListRenderer = new GameListRenderer(gameListAccessor);
     }
 
-    public void promptAndWait(String context)
-    {
-        inPrompt = true;
-
+    public void promptAndWait(String context) {
         System.out.printf("CHESS [%s] $ ", context);
         reader.read();
-
-        while (!pendingMessages.isEmpty()) {
-            ScreenMessage message = pendingMessages.poll();
-            if(message.isError) {
-                printErrorMessage(message.text);
-            } else {
-                printNotification(message.text);
-            }
-        }
-
-        if(pendingBoard != null) {
-            boardRenderer.render(pendingBoard.data, pendingBoard.color);
-            pendingBoard = null;
-        }
-
-        inPrompt = false;
     }
 
     public String firstWordEntered() {
@@ -67,15 +41,7 @@ public class BufferedRenderer implements Closeable {
     }
 
     public void update(String message) {
-        if(inPrompt) {
-            pendingMessages.add(new ScreenMessage(message, false));
-        } else {
-            printNotification(message);
-        }
-    }
-
-    private static void printNotification(String text) {
-        System.out.println(text);
+        System.out.println(message);
         System.out.println();
     }
 
@@ -93,11 +59,7 @@ public class BufferedRenderer implements Closeable {
     }
 
     public void board(ChessBoard board, ChessGame.TeamColor viewerColor) {
-        if(inPrompt) {
-            this.pendingBoard = new GameInfo(board, viewerColor);
-        } else {
-            boardRenderer.render(board, viewerColor);
-        }
+        boardRenderer.render(board, viewerColor);
     }
 
     public void gamesList() {
@@ -109,17 +71,9 @@ public class BufferedRenderer implements Closeable {
         System.out.println();
     }
 
-    public void error(String text) {
-        if(inPrompt) {
-            pendingMessages.add(new ScreenMessage(text, true));
-        } else {
-            printErrorMessage(text);
-        }
-    }
-
-    private static void printErrorMessage(String text) {
+    public void error(String message) {
         System.out.print(EscapeSequences.SET_TEXT_COLOR_RED);
-        System.out.printf(">>> %s%n", text);
+        System.out.printf(">>> %s%n", message);
         System.out.println();
         System.out.print(EscapeSequences.RESET_TEXT_COLOR);
     }
@@ -127,11 +81,5 @@ public class BufferedRenderer implements Closeable {
     @Override
     public void close() throws IOException {
         reader.close();
-    }
-
-    private record ScreenMessage(String text, boolean isError) {
-    }
-
-    private record GameInfo(ChessBoard data, ChessGame.TeamColor color) {
     }
 }
