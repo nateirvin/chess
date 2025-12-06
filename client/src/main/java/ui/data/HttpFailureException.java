e7879ce20dbd35698e179ee0d6daf792f1c113d7
Nate Irvin <irvinn@byu.edu>
2025-12-05 23:29:37 -0700

    beefing up error handling

--- client/src/main/java/ui/data/HttpFailureException.java
+++ client/src/main/java/ui/data/HttpFailureException.java
@@ -3,7 +3,13 @@ package ui.data;
 public class HttpFailureException extends Exception {
     private final int statusCode;
 
-    public HttpFailureException(int statusCode) {
+    public HttpFailureException(int statusCode, String message) {
+        super(message);
+        this.statusCode = statusCode;
+    }
+
+    public HttpFailureException(int statusCode, Exception baseException) {
+        super("HTTP fault", baseException);
         this.statusCode = statusCode;
     }
 
